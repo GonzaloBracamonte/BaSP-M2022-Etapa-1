3ab484c73b96f9e0d145bdd6b05fa1531c2db9ad
Gonzalo Bracamonte <braca95gonza@gmail.com>
2022-04-15 17:55:25 -0300

    BaSP-2022: ex01-variables-and-operators-B

--- semana-05/excercises/01-variables-and-operators.js
+++ semana-05/excercises/01-variables-and-operators.js
@@ -3,3 +3,10 @@ var b = 17;
 var c;
 c = a + b;
 console.log("ex01-variables and operators-A = " + c)
+
+
+var a = "Hola ";
+var b = "Mundo";
+var c;
+c = a + b
+console.log("ex01-variables and operators-B = " + c)
