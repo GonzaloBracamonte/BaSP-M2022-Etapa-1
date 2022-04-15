95a4a0c9971c092e7f98d42c1d10ba3274a438ad
Gonzalo Bracamonte <braca95gonza@gmail.com>
2022-04-15 19:59:20 -0300

    BaSP-2022: ex03-arrays-E

--- semana-05/excercises/03-arrays.js
+++ semana-05/excercises/03-arrays.js
@@ -2,8 +2,8 @@ const months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio",
 "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 console.log("ex03-strings-A = " + months[5] + " y " + months[11]);
 
-//months.sort();
-//console.log("ex03-strings-B = " + months);
+//const sort = months.sort();
+//console.log("ex03-strings-B = " + sort);
 
 months.unshift("Primer elemento");
 months.push("Ultimo elemento");
@@ -12,3 +12,7 @@ console.log("ex03-strings-C = " + months)
 months.shift();
 months.pop();
 console.log("ex03-strings-D = " + months)
+
+const reverse = months.reverse();
+console.log("ex03-strings-E = " + reverse)
+
