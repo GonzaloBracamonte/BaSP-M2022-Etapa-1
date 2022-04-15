ed4f273b568b5f47de57dd9e58c0240e6e6fdfc3
Gonzalo Bracamonte <braca95gonza@gmail.com>
2022-04-15 18:05:51 -0300

    BaSP-2022: ex02-strings-E

--- semana-05/excercises/02-strings.js
+++ semana-05/excercises/02-strings.js
@@ -11,4 +11,9 @@ console.log("ex02-strings-C = " + b);
 
 var a = "eSTO ES UN EJEMPLO DE UPPER Y LOWER CASE";
 var b = a.substring(0,1).toUpperCase() + a.substring(1,10).toLowerCase();
-console.log("ex02-strings-C = " + b)
+console.log("ex02-strings-D = " + b)
+
+var a = "Ejemplo de mostrar la posicion de un caracter especifico";
+var b = " ";
+var c = a.indexOf(b);
+console.log("ex02-strings-E = " + c);
