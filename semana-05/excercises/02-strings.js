a380e7fff3ea9fd2d6173e1e9c2a769cfb0d34a0
Gonzalo Bracamonte <braca95gonza@gmail.com>
2022-04-15 18:03:30 -0300

    BaSP-2022: ex02-strings-C

--- semana-05/excercises/02-strings.js
+++ semana-05/excercises/02-strings.js
@@ -3,4 +3,8 @@ console.log("ex02-strings-A = " + a.toLocaleUpperCase())
 
 var a = "Como mostrar cierta cantidad de caracteres"
 var b = a.substring(0,5)
-console.log("ex02-strings-A = " + b)
+console.log("ex02-strings-B = " + b)
+
+var a = "Otra forma de mostrar cierta cantidad de caracteres";
+var b = a.substring(51 , 48);
+console.log("ex02-strings-C = " + b);
