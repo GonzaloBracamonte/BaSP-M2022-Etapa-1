19b7fd1344907fe5edc358312ff03eac85bdd38b
Gonzalo Bracamonte <braca95gonza@gmail.com>
2022-04-15 18:00:40 -0300

    BaSP-2022: ex02-strings-B

--- semana-05/excercises/02-strings.js
+++ semana-05/excercises/02-strings.js
@@ -1,2 +1,6 @@
 var a = "este es el ejemplo de uper case"
 console.log("ex02-strings-A = " + a.toLocaleUpperCase())
+
+var a = "Como mostrar cierta cantidad de caracteres"
+var b = a.substring(0,5)
+console.log("ex02-strings-A = " + b)
