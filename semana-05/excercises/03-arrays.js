4820365566700b5c3690e80db9c3faf042dec7b0
Gonzalo Bracamonte <braca95gonza@gmail.com>
2022-04-16 09:37:35 -0300

    BaSP-2022: Correction of variable declaration

--- semana-05/excercises/03-arrays.js
+++ semana-05/excercises/03-arrays.js
@@ -1,23 +1,38 @@
-const months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", 
+var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", 
 "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 console.log("ex03-strings-A = " + months[5] + " y " + months[11]);
 
-//var sort = months.sort();
-//console.log("ex03-strings-B = " + sort);
+var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", 
+"Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+var sort = months.sort();
+console.log("ex03-strings-B = " + sort);
 
-//months.unshift("Primer elemento");
-//months.push("Ultimo elemento");
-//console.log("ex03-strings-C = " + months)
 
-//months.shift();
-//months.pop();
-//console.log("ex03-strings-D = " + months)
+var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", 
+"Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+months.unshift("Primer elemento");
+months.push("Ultimo elemento");
+console.log("ex03-strings-C = " + months)
 
-//const reverse = months.reverse();
-//console.log("ex03-strings-E = " + reverse)
 
-//const join = months.join("-")
-//console.log("ex03-strings-F = " + join);
+var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", 
+"Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+months.shift();
+months.pop();
+console.log("ex03-strings-D = " + months)
 
+var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", 
+"Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+const reverse = months.reverse();
+console.log("ex03-strings-E = " + reverse)
+
+var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", 
+"Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+const join = months.join("-")
+console.log("ex03-strings-F = " + join);
+
+
+var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", 
+"Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
 var slice = months.slice(4,11);
 console.log("ex03-strings-G = " + slice);
