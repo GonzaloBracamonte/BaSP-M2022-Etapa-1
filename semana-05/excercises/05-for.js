5934124b9b0a5164001d2b0ec5b14a68c5290b21
Gonzalo Bracamonte <braca95gonza@gmail.com>
2022-04-18 20:53:54 -0300

    BaSP-2022: ex05-for-B

--- semana-05/excercises/05-for.js
+++ semana-05/excercises/05-for.js
@@ -3,15 +3,13 @@ for (let i = 0; i < array.length; i++) {
     console.log("ex05-for-A = " + array[i]);
 }
 
-/*var Array = ["arreglo", "iteracion", "programacion", "for", "curso",]
+var Array = ["arreglo", "iteracion", "programacion", "for", "curso",]
 var cont = 0;
 for (let i = 0; i < 5; i++){
-    i++
-    if (Array[i] != Array[i].charAt(0).toUpperCase() + Array[i].substring(1)) {
-        Array[i].charAt(0).toUpperCase();
-    }
+    cont = i + 1;
+    Array[i] = Array[i].charAt(0).toUpperCase() + Array[i].substring(1)
 }
-console.log("ex05-for-B = " + i + " palabras han sido cambiadas");*/
+console.log("ex05-for-B = " + cont + " palabras han sido cambiadas");
 
 var sentence = [];
 for (let i = 0; i < array.length; i++) {
