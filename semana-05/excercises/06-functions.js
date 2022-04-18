426a806bcbf5a60fed42f78f78697ccafd413014
Gonzalo Bracamonte <braca95gonza@gmail.com>
2022-04-18 17:04:10 -0300

    BaSP-2022: ex06-functions-C

--- semana-05/excercises/06-functions.js
+++ semana-05/excercises/06-functions.js
@@ -15,3 +15,15 @@ function operator(num1, num2){
 }
 var answer = operator(2,6)
 console.log("ex06-functions-B = " + answer)
+
+function isInt(value) {
+  if (value === parseInt(value, 10)){
+    return true
+  }
+  else{
+    return false
+  }
+}
+var answer = isInt(30)
+console.log("ex06-functions-C = " + answer)
+
