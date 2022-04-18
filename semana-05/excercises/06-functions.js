c5c1f67300306b54f321448bb1c20df19002eb84
Gonzalo Bracamonte <braca95gonza@gmail.com>
2022-04-18 16:23:52 -0300

    BaSP-2022: ex06-functions-B

--- semana-05/excercises/06-functions.js
+++ semana-05/excercises/06-functions.js
@@ -1,5 +1,17 @@
 function operator(num1, num2){
   return num1 + num2
 }
-const answer = operator(6,22)
+var answer = operator(6,22)
 console.log("ex06-functions-A = " + answer)
+var answer = operator(6,22)
+
+function operator(num1, num2){
+  if (isNaN(num1) || isNaN(num2)) {
+    console.log("Uno de los campos es incorrecto")
+    return NaN
+  } else{
+    return num1 + num2
+  }
+}
+var answer = operator(2,6)
+console.log("ex06-functions-B = " + answer)
