4af9dc96bf1fc4ff3c3a2884b004bca0c71045f4
Gonzalo Bracamonte <braca95gonza@gmail.com>
2022-04-19 00:16:13 -0300

    BaSP-2022: ex06-functions-D

--- semana-05/excercises/06-functions.js
+++ semana-05/excercises/06-functions.js
@@ -1,7 +1,6 @@
 function operator(num1, num2){
   return num1 + num2
 }
-var answer = operator(6,22)
 console.log("ex06-functions-A = " + answer)
 var answer = operator(6,22)
 
@@ -13,7 +12,7 @@ function operator(num1, num2){
     return num1 + num2
   }
 }
-var answer = operator(2,6)
+var answer = operator(21,64)
 console.log("ex06-functions-B = " + answer)
 
 function isInt(value) {
@@ -26,4 +25,23 @@ function isInt(value) {
 }
 var answer = isInt(30)
 console.log("ex06-functions-C = " + answer)
+
+var answer = operator(7, 2)
+function operator(num1, num2){
+  if (isNaN(num1) || isNaN(num2)) {
+    console.log("Uno de los campos es incorrecto")
+    return NaN
+  } 
+  else if (num1 === parseInt(num1, 10) &&
+        num2 === parseInt(num2, 10))
+  {
+    return num1 + num2
+  } 
+  else if (num1 === parseFloat(num1, 10) ||
+        num2 === parseFloat(num2, 10))
+        {
+          console.log("ERROR" +" "+ Math.round(num1) + " " + Math.round(num2))
+        }
+      }
+      console.log("ex06-functions-D = " + answer)
 
