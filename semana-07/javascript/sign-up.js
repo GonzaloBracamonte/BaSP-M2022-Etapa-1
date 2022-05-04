b6c7df67db69f37ec7ffd65e23ef8e2c6ab0b047
Gonzalo Bracamonte <braca95gonza@gmail.com>
2022-05-03 22:09:57 -0300

    BaSP-W07-2022: Correction of last week's errors

--- semana-07/javascript/sign-up.js
+++ semana-07/javascript/sign-up.js
@@ -10,11 +10,11 @@ window.onload = function () {
   var email = document.getElementById("email");
   var password = document.getElementById("password");
   var rePassword = document.getElementById("reCheckPassword");
+  var storage = localStorage;
 
   var fNameError = document.getElementById("fNameP");
   var lNameError = document.getElementById("lNameP");
   var dniError = document.getElementById("dniP");
-  // var  dOfBirthError= document.getElementById("dateOfBirth");
   var pNumberError = document.getElementById("pNumberP");
   var addressError = document.getElementById("addressP");
   var cityError = document.getElementById("cityP");
@@ -23,46 +23,68 @@ window.onload = function () {
   var passwordError = document.getElementById("passwordP");
   var rePasswordError = document.getElementById("rePasswordP");
 
-  // localStorage.setItem("name", firstName);
-  // localStorage.setItem("lastName", lastName);
-  // localStorage.setItem("dni", dni);
-  // localStorage.setItem("dob", "29-07-1995");
-  // localStorage.setItem("phone", phone);
-  // localStorage.setItem("address", address);
-  // localStorage.setItem("city", city);
-  // localStorage.setItem("zip", postalCode);
-  // localStorage.setItem("email", emailLog);
-  // localStorage.setItem("password", password);
-
   fName.addEventListener("focus", fNameFocus, true);
   fName.addEventListener("blur", fNameBlur, true);
+  fName.addEventListener("focusout", function () {
+    storage.setItem("name", fName.value);
+  });
 
   lName.addEventListener("focus", lNameFocus, true);
   lName.addEventListener("blur", lNameBlur, true);
+  lName.addEventListener("focusout", function () {
+    storage.setItem("lastName", lName.value);
+  });
 
   dni.addEventListener("focus", dniFocus, true);
   dni.addEventListener("blur", dniBlur, true);
-
-  // dOfBirth.addEventListener('focus', dOfBirthFocus, true);
-  // dOfBirth.addEventListener('blur', dOfBirthBlur, true);
+  dni.addEventListener("focusout", function () {
+    storage.setItem("dni", dni.value);
+  });
+  dOfBirth.addEventListener("focusout", function () {
+    storage.setItem("dob", dOfBirth.value);
+  });
+  function toMonthDayYear(dateToConv) {
+    [year, month, day] = dateToConv.split("-");
+    var dateMDY = [month, day, year].join("/");
+    return dateMDY;
+  }
+  toMonthDayYear(dOfBirth.value);
 
   pNumber.addEventListener("focus", pNumberFocus, true);
   pNumber.addEventListener("blur", pNumberBlur, true);
+  pNumber.addEventListener("focusout", function () {
+    storage.setItem("phone", pNumber.value);
+  });
 
   address.addEventListener("focus", addressFocus, true);
   address.addEventListener("blur", addressBlur, true);
+  address.addEventListener("focusout", function () {
+    storage.setItem("address", address.value);
+  });
 
   city.addEventListener("focus", cityFocus, true);
   city.addEventListener("blur", cityBlur, true);
+  city.addEventListener("focusout", function () {
+    storage.setItem("city", city.value);
+  });
 
   pCode.addEventListener("focus", pCodeFocus, true);
   pCode.addEventListener("blur", pCodeBlur, true);
+  pCode.addEventListener("focusout", function () {
+    storage.setItem("zip", pCode.value);
+  });
 
   email.addEventListener("focus", emailFocus, true);
   email.addEventListener("blur", emailBlur, true);
+  email.addEventListener("focusout", function () {
+    storage.setItem("email", email.value);
+  });
 
   password.addEventListener("focus", passwordFocus, true);
   password.addEventListener("blur", passwordBlur, true);
+  password.addEventListener("focusout", function () {
+    storage.setItem("password", password.value);
+  });
 
   rePassword.addEventListener("focus", rePasswordFocus, true);
   rePassword.addEventListener("blur", rePasswordBlur, true);
@@ -72,7 +94,7 @@ window.onload = function () {
     fNameError.style.visibility = "hidden";
   }
   function fNameBlur() {
-    if (fName == null || fName.value.length < 3 || /^\s+$/.test(fName.value)) {
+    if (fName == null || fName.value.length < 3 || !isNaN(fName)) {
       document.getElementById("firstName").style.backgroundColor = "pink";
       fNameError.style.visibility = "visible";
       fNameError.style.color = "red";
@@ -84,7 +106,7 @@ window.onload = function () {
     lNameError.style.visibility = "hidden";
   }
   function lNameBlur() {
-    if (lName == null || lName.value.length < 3 || /^\s+$/.test(lName.value)) {
+    if (lName == null || lName.value.length < 3 || !isNaN(lName.value)) {
       document.getElementById("lastName").style.backgroundColor = "pink";
       lNameError.style.visibility = "visible";
       lNameError.style.color = "red";
@@ -97,20 +119,12 @@ window.onload = function () {
   }
 
   function dniBlur() {
-    if (!/^\d{8,12}$/.test(dni.value) || isNaN(dni.value)) {
+    if (isNaN(dni)) {
       document.getElementById("dni").style.backgroundColor = "pink";
       dniError.style.visibility = "visible";
       dniError.style.color = "red";
     }
   }
-  // function dOfBirthFocus(){
-  //   document.getElementById("dateOfBirth").style.backgroundColor = "";
-  //   dOfBirthError.style.visibility = "hidden";
-  // }
-
-  // function dOfBirthBlur(){
-
-  // }
 
   function pNumberFocus() {
     document.getElementById("phone").style.backgroundColor = "";
@@ -213,6 +227,19 @@ window.onload = function () {
       rePasswordError.style.color = "red";
     }
   }
+  function getValues() {
+    fName.value = storage.getItem("name");
+    lName.value = storage.getItem("lastName");
+    dni.value = storage.getItem("dni");
+    dOfBirth.value = storage.getItem("dob");
+    pNumber.value = storage.getItem("phone");
+    address.value = storage.getItem("address");
+    city.value = storage.getItem("city");
+    pCode.value = storage.getItem("zip");
+    email.value = storage.getItem("email");
+    password.value = storage.getItem("password");
+  }
+  document.addEventListener("DOMContentLoaded", getValues);
 };
 
 function validationName() {
@@ -220,49 +247,36 @@ function validationName() {
   if (!isNaN(firstName) || firstName == null || firstName.length < 3) {
     return false;
   } else {
-    localStorage.setItem("name", firstName);
     return true;
   }
 }
 
 function validationLastName() {
   lastName = document.getElementById("lastName").value;
-  if (lastName == null || lastName.length < 3 || !isNaN(lastName)) {
+  if (lastName == null || lastName.length < 5 || !isNaN(lastName)) {
     return false;
   } else {
-    localStorage.setItem("lastName", lastName);
     return true;
   }
 }
 
 function validationDni() {
   dni = document.getElementById("dni").value;
-  if (dni < 9 || isNaN(dni)) {
+  if (dni.length > 9 || isNaN(dni)) {
     return false;
   } else {
-    localStorage.setItem("dni", dni);
     return true;
   }
 }
 
 function validationDateOfBirth() {
-  // var dOfBirth = document.getElementById("dateOfBirth").value;
-  // var [year, month, day] = dOfBirth.split("/");
-  // var isoFormattedStr = `${day}/${month}/${year}`;
-  // var date = new Date(isoFormattedStr);
-  // var timestamp = date.getTime();
-  // if (typeof timestamp !== "number" || Number.isNaN(timestamp))
-  // {
-  //   console.log("Fecha error")
-  //   return false;
-  // } else if (new Date(dOfBirth.value).getTime() > new Date().getTime()) {
-  //   console.log("Fecha invalida")
-  //   return false;
-  // }
-  // console.log("Salio todo bien")
-  // return isoFormattedStr;
-  localStorage.setItem("dob", "12/31/1995");
-  return true;
+  var dOfBirth = document.getElementById("dateOfBirth").value;
+  var [year, month, day] = dOfBirth.split("-");
+  var isoFormattedStr = `${year}/${month}/${day}`;
+  var date = new Date(isoFormattedStr);
+  var currentDate = new Date(Date.now());
+  console.log(dOfBirth);
+  return currentDate >= date;
 }
 
 function validationPhoneNumber() {
@@ -270,7 +284,6 @@ function validationPhoneNumber() {
   if (phone < 10 || isNaN(phone)) {
     return false;
   } else {
-    localStorage.setItem("phone", phone);
     return true;
   }
 }
@@ -280,7 +293,6 @@ function validationAddress() {
   if (address == null || address.length < 5) {
     return false;
   } else {
-    localStorage.setItem("address", address);
     return true;
   }
 }
@@ -290,18 +302,15 @@ function validationCity() {
   if (city == null || city.length < 3) {
     return false;
   } else {
-    localStorage.setItem("city", city);
     return true;
   }
 }
 
 function validationPostalCode() {
-  localStorage.getItem("zip");
   postalCode = document.getElementById("pc").value;
   if (postalCode < 5 || isNaN(postalCode)) {
     return false;
   } else {
-    localStorage.setItem("zip", postalCode);
     return true;
   }
 }
@@ -311,7 +320,6 @@ function validationEmail() {
   if (
     /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i.test(emailLog)
   ) {
-    localStorage.setItem("email", emailLog);
     return true;
   } else {
     return false;
@@ -323,7 +331,6 @@ function passwordCheck() {
   if (password == null || password.length < 3) {
     return false;
   } else {
-    localStorage.setItem("password", password);
     return true;
   }
 }
@@ -405,3 +412,41 @@ function validationForm() {
     return true;
   }
 }
+// +
+//   "?name=" +
+//   firstName.value +
+//   "&lastName=" +
+//   lastName.value +
+//   "&dni=" +
+//   dni.value +
+//   "&dob=" +
+//   validationDateOfBirth(toMonthDayYear.value) +
+//   "&phone=" +
+//   phone.value +
+//   "&address=" +
+//   address.value +
+//   "&city=" +
+//   city.value +
+//   "&zip=" +
+//   postalCode.value +
+//   "&email=" +
+//   emailLog.value +
+//   "&password=" +
+//   password.value
+// function validateDate(dateToValidate){
+
+//   var [year, month, day] = dateToValidate.split("-");
+//   var isoFormattedStr = `${year}/${month}/${day}`;
+//   var date = new Date(isoFormattedStr);
+//   var currentDate = new Date(Date.now());
+
+//   return currentDate >= date
+// }
+// var currentDate = new Date(Date.now());
+// function toMonthDayYear(dateToConv){
+
+//   [year, month, day] = dateToConv.split('-');
+//   var dateMDY = [month, day, year].join('/')
+
+//   return dateMDY
+// }
